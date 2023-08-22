f2a9f333b28bac497c380b036c2bf3102c97d46f
Kantapat Radchawatr <kantapat.r@ku.th>
2023-08-22 12:02:12 +0700

    Lab5 Spring boot and spring web API

---
+++ src/main/java/ku/cs/restaurant/controller/RestaurantController.java
@@ -0,0 +1,51 @@
+package ku.cs.restaurant.controller;
+
+import ku.cs.restaurant.entity.Restaurant;
+import ku.cs.restaurant.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class RestaurantController {
+
+    @Autowired
+    private RestaurantService service;
+
+    @GetMapping("/restaurant")
+    public List<Restaurant> getAllRestaurant() {
+        return service.getAll();
+    }
+
+    @GetMapping("/restaurant/{id}")
+    public Restaurant getRestaurantById(@PathVariable UUID id){
+        return service.getRestaurabtById(id);
+    }
+
+    @GetMapping("/restaurant/name/{name}")
+    public Restaurant getRestaurantByName(@PathVariable String name){
+        return service.getRestaurabtByName(name);
+    }
+
+    @GetMapping("/restaurant/location/{location}")
+    public List<Restaurant> getRestaurantByLocation(@PathVariable String location){
+        return service.getRestaurantByLocation(location);
+    }
+
+    @PostMapping("/restaurant")
+    public Restaurant create(@RequestBody Restaurant restaurant) {
+        return service.create(restaurant);
+    }
+
+    @PutMapping("/restaurant")
+    public Restaurant update(@RequestBody Restaurant restaurant){
+        return service.update(restaurant);
+    }
+
+    @DeleteMapping("/restaurant/{id}")
+    public Restaurant delete(@PathVariable UUID id){
+        return service.delete(id);
+    }
+}
